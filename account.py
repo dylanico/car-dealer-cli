06f4eb0aaa60ac6b8cfd59bd533ef7555de55508
Anirudh Singh <creator.anirudhsingh@gmail.com>
2022-07-21 13:14:26 +0530

    Basic Changes

---
+++ account.py
@@ -0,0 +1,13 @@
+# Coins at the beginning of the game
+coins = 100
+
+# Energy at the beginning of the game
+energy = 100
+
+# Storage at the beginning of the game (Maximum no. of cars that can be stored in Garage)
+storage = 1
+
+# Purchased cars will be stored here as a nested Dictionary
+garage = {
+
+}
---
+++ cars.py
@@ -0,0 +1,5 @@
+cars = {
+    'Mercedes' : 1000,
+    'Audi' : 2000,
+    'DBS Superleggera': 1600,
+}
